b89bab65f7f0b9300f2681badb5b49ce844d018a
djembuka <djembuka@gmail.com>
2024-11-11 17:56:15 +0300

    Mobile footer

--- source/components/footer_delunet/script.js
+++ source/components/footer_delunet/script.js
@@ -1,30 +1,45 @@
 window.addEventListener('load', () => {
-  initMap();
+  document.getElementById('footerMap').style.transition = 'height .3s ease';
 
-  async function initMap() {
-    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
-    await ymaps3.ready;
+  if (document.querySelector('.bj-page-footer-delunet__map-button')) {
+    document
+      .querySelector('.bj-page-footer-delunet__map-button')
+      .addEventListener('click', (e) => {
+        e.preventDefault();
+        document
+          .querySelector('.bj-page-footer-delunet__map')
+          .classList.toggle('bj-page-footer-delunet__map--show');
+      });
+  }
 
-    const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+  if (ymaps3 && document.getElementById('footerMap')) {
+    initMap();
 
-    // Иницилиазируем карту
-    const map = new YMap(
-      // Передаём ссылку на HTMLElement контейнера
-      document.getElementById('footerMap'),
+    async function initMap() {
+      // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+      await ymaps3.ready;
 
-      // Передаём параметры инициализации карты
-      {
-        location: {
-          // Координаты центра карты
-          center: [37.588144, 55.733842],
+      const { YMap, YMapDefaultSchemeLayer } = ymaps3;
 
-          // Уровень масштабирования
-          zoom: 10,
-        },
-      }
-    );
+      // Иницилиазируем карту
+      const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('footerMap'),
 
-    // Добавляем слой для отображения схематической карты
-    map.addChild(new YMapDefaultSchemeLayer());
+        // Передаём параметры инициализации карты
+        {
+          location: {
+            // Координаты центра карты
+            center: [37.588144, 55.733842],
+
+            // Уровень масштабирования
+            zoom: 10,
+          },
+        }
+      );
+
+      // Добавляем слой для отображения схематической карты
+      map.addChild(new YMapDefaultSchemeLayer());
+    }
   }
 });
